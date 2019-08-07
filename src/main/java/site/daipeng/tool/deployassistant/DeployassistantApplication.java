4345993b0f1e8edb439298dbcada34013d47020f
daipeng <daipeng@yuewen.com>
2019-08-07 13:10:59 +0800

    1 添加启动方法

---
+++ src/main/java/site/daipeng/tool/deployassistant/DeployassistantApplication.java
@@ -0,0 +1,13 @@
+package site.daipeng.tool.deployassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployassistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployassistantApplication.class, args);
+    }
+
+}
---
+++ src/main/java/site/daipeng/tool/deployassistant/controller/AssistantController.java
@@ -0,0 +1,74 @@
+package site.daipeng.tool.deployassistant.controller;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author daipeng
+ * @date 2019/8/6 21:54
+ * @description
+ */
+@RestController
+@RequestMapping("deploy")
+public class AssistantController {
+    private String defaultScript = "~/start.sh";
+
+    private ExecutorService es = Executors.newFixedThreadPool(10);
+
+    @GetMapping("")
+    public String deploy(@RequestParam(required = false, defaultValue = "") String script) throws  Exception {
+        String runScript = defaultScript;
+        if (!StringUtils.isEmpty(script)) {
+            runScript = script;
+        }
+        final  Process process = Runtime.getRuntime().exec(defaultScript);
+
+        es.submit(() -> {
+            try {
+                process.waitFor();
+
+            } catch (Exception e) {
+                System.err.println(e.getStackTrace());
+
+            }
+        });
+
+        es.submit(() -> {
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));) {
+
+                String line = null;
+                while ((line = in.readLine()) != null) {
+                    System.err.println(line);
+                }
+            } catch (Exception e) {
+
+            }
+        });
+        es.submit(() -> {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getErrorStream()));) {
+
+                String line = null;
+                while ((line = in.readLine()) != null) {
+                    System.err.println(line);
+                }
+            } catch (Exception e) {
+
+            }
+        });
+
+
+        return "success";
+    }
+
+}
