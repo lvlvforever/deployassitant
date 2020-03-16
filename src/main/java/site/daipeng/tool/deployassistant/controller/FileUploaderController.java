22b9a9d78ed62f6ce88c279bb6bea08a31bf72cc
lvlv <lvlvbuaa@163.com>
2020-03-16 13:33:06 +0800

    123

---
+++ src/main/java/site/daipeng/tool/deployassistant/controller/FileUploaderController.java
@@ -0,0 +1,7 @@
+package site.daipeng.tool.deployassistant.controller;
+
+/**
+ * Created by lvlvforever on 2020/3/16.
+ */
+public class FileUploaderController {
+}
--- src/main/java/site/daipeng/tool/deployassistant/controller/UploaderController.java
+++ src/main/java/site/daipeng/tool/deployassistant/controller/UploaderController.java
@@ -7,9 +7,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotNull;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
 
 /**
  * Created by lvlvforever on 2020/2/22.
@@ -17,8 +19,12 @@ import java.util.Map;
 @RestController
 @RequestMapping("file")
 public class UploaderController {
+
     @Value("${file.upload.dir}")
     private String dir;
+
+    @Value("${tmpfile.upload.dir}")
+    private String tmpDir;
 
     @CrossOrigin(allowCredentials = "true", maxAge = 3600)
     @PostMapping(value = "/upload")
@@ -36,5 +42,57 @@ public class UploaderController {
         data.put("fileId", fileName);
         return data;
     }
+    @CrossOrigin(allowCredentials = "true", maxAge = 3600)
+    @PostMapping(value = "/uploadTmpFile")
+    public Map<String, Object> uploadTmpFile(MultipartFile file) throws IOException {
 
+        Map<String, Object> data = new HashMap<>();
+        String originalName = file.getOriginalFilename();
+        String fileName = queryNextFileId() + "." + originalName.substring(originalName.indexOf(".") + 1);
+
+        File dest = new File(tmpDir + File.separator + fileName);
+        try (InputStream inputStream = file.getInputStream()) {
+            FileUtils.copyToFile(inputStream, dest);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        data.put("fileId", fileName);
+        return data;
+    }
+    @GetMapping(value = "/queryFiles")
+    public List<String> queryFiles() throws IOException {
+
+        List<String> data = new ArrayList<>();
+        File file = new File(tmpDir);
+        File[] files = file.listFiles();
+        if (files == null) {
+            return data;
+
+        }
+        for (File f : files) {
+            String fileName = f.getName();
+            data.add(fileName);
+        }
+        return data;
+    }
+
+    private int queryNextFileId() throws IOException {
+        File file = new File(tmpDir);
+        File[] files = file.listFiles();
+        PriorityQueue<Integer> ids = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        if (files == null) {
+            return 1;
+        }
+        for (File f : files) {
+            String fileName = f.getName();
+            String id = fileName.substring(0, fileName.indexOf("."));
+            ids.add(Integer.valueOf(id));
+        }
+        return ids.peek() + 1;
+    }
 }
